857d90bb3a7877f06f106042d4bbc23573fbf131
rahulk7609 <rahuldeveloper32@gmail.com>
2021-10-14 14:28:23 -0400

    second commit

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
 
 const App: React.FC= () =>  {
+  const [todo, setTodo] = useState<string>("");
+
+  console.log(todo);
+
   return (
     <div className="App">
       <span className = "heading">TypeScript-Todo</span>
-      <InputFeild />
+      <InputFeild todo ={todo} setTodo ={setTodo} />
     </div>
   );
 }
--- react-todo/src/components/InputFeild.tsx
+++ react-todo/src/components/InputFeild.tsx
@@ -1,11 +1,20 @@
 import React from "react"
 import "./styles.css"
 
-const InputFeild = () => {
+interface Props {
+    todo : string;
+    setTodo : React.Dispatch<React.SetStateAction<string>>;
+}
+
+
+const InputFeild = ({todo ,setTodo}: Props) => {
     return(
         <div>
             <form className= "input">
-                <input className= "input_box" type= "input" placeholder= "Enter a task"></input> 
+                <input className= "input_box" 
+                    value = {todo}
+                    onChange = {(e) => setTodo(e.target.value)}
+                type= "input" placeholder= "Enter a task"></input> 
                 <button type="submit" className="input_submit">GO</button>
             </form>           
         </div>
